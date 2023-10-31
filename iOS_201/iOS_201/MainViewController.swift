f0fcea313a9b46b99aa9aafeeb68e79cd72ccc4b
Youjunyong <hellgey777@naver.com>
2023-10-31 19:30:40 +0900

    #1 라이브러리 설치, git Ignore, CodeBase UI 설정

--- iOS_201/iOS_201/MainViewController.swift
+++ iOS_201/iOS_201/MainViewController.swift
@@ -6,15 +6,13 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = .red
+        self.view.backgroundColor = .systemBackground
     }
-
-
 }
 
