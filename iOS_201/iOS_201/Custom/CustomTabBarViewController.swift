41db5577378a11a310d5c280d425316be662e9ec
Youjunyong <hellgey777@naver.com>
2023-11-03 16:01:45 +0900

    Feat - Add TabBar and 4 screens for the corresponding tab #4

---
+++ iOS_201/iOS_201/Custom/CustomTabBarViewController.swift
@@ -0,0 +1,78 @@
+//
+//  CustomTabBarViewController.swift
+//  iOS_201
+//
+//  Created by 유준용 on 2023/11/03.
+//
+
+import UIKit
+
+class CustomTabBarViewController: UITabBarController {
+
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTabBar()
+    }
+    
+    // MARK: - Configure UI
+    
+    private func configureTabBar(){
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        UITabBar.appearance().backgroundColor = .black02
+        tabBar.isTranslucent = false
+        setViewControllers([createHomeTab(), createStudyListTab(), createMessageTab(), createMyPageTab()], animated: true)
+    }
+    
+    // MARK: - Set TabBar's ViewControllers
+    
+    private func createHomeTab() -> UINavigationController {
+        let item = configureTabBarItemTitle(title: "홈", imageName: "homeTab")
+        let VC = HomeViewController()
+        VC.tabBarItem = item
+        return UINavigationController(rootViewController: VC)
+    }
+
+    
+    private func createStudyListTab() -> UINavigationController {
+        let item = configureTabBarItemTitle(title: "스터디목록", imageName: "studyListTab")
+        let VC = StudyListViewController()
+        VC.tabBarItem = item
+        return UINavigationController(rootViewController: VC)
+    }
+    
+    private func createMessageTab() -> UINavigationController{
+        let item = configureTabBarItemTitle(title: "쪽지", imageName: "messageTab")
+        let VC = MessageViewController()
+        VC.tabBarItem = item
+        return UINavigationController(rootViewController: VC)
+
+    }
+    private func createMyPageTab() -> UINavigationController{
+        let item = configureTabBarItemTitle(title: "마이페이지", imageName: "myPageTab")
+        let VC = MypageViewController()
+        VC.tabBarItem = item
+        return UINavigationController(rootViewController: VC)
+    }
+    
+    private func configureTabBarItemTitle(title:String, imageName: String ) -> UITabBarItem{
+        let item = UITabBarItem(title: title, image: UIImage(named: imageName)?.withRenderingMode(.alwaysOriginal),
+                                selectedImage: UIImage(named: imageName + "Fill")?.withRenderingMode(.alwaysOriginal))
+        item.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.clear], for: .normal)
+        item.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.green06], for: .selected)
+        return item
+    }
+    
+    //MARK: - TabBarViewController Methods
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        guard let itemIndex = tabBar.items?.firstIndex(of: item)?.distance(to: 0) else {return}
+        let index = -itemIndex
+        let icon = tabBar.subviews[index+1].subviews.last as? UIImageView
+        item.image?.ciImage?.transformed(by: CGAffineTransform(rotationAngle: 0.5))
+        UIImpactFeedbackGenerator(style: .light).impactOccurred()
+    }
+}
---
+++ iOS_201/iOS_201/Extension/UIColor.swift
@@ -0,0 +1,36 @@
+//
+//  UIColor.swift
+//  iOS_201
+//
+//  Created by 유준용 on 2023/11/03.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    ///#39D353 40%
+    static let green01 = UIColor(named: "green01")
+    ///#39D353 90%
+    static let green02 = UIColor(named: "green02")
+    ///#016D32
+    static let green03 = UIColor(named: "green03")
+    ///#26A641
+    static let green04 = UIColor(named: "green04")
+    ///#3AD353
+    static let green05 = UIColor(named: "green05")
+    ///#7ADC4B
+    static let green06 = UIColor(named: "green06")
+    ///#0D1117
+    static let black01 = UIColor(named: "black01")
+    ///#181B21
+    static let black02 = UIColor(named: "black02")
+    ///#272C33
+    static let black03 = UIColor(named: "black03")
+    ///#C5C5C5
+    static let grey01  = UIColor(named: "grey01")
+    ///#EB7A7A
+    static let red01   = UIColor(named: "red01")
+    ///#9BB9F2
+    static let blue01  = UIColor(named: "blue01")
+}
