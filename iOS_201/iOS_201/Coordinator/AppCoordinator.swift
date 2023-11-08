12035216af4461a175851f34c22f65e9b227bdf7
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2023-11-08 22:25:43 +0900

    스플래쉬뷰 스토리보드로 추가

--- iOS_201/iOS_201/Coordinator/AppCoordinator.swift
+++ iOS_201/iOS_201/Coordinator/AppCoordinator.swift
@@ -1,8 +1,42 @@
-//
-//  AppCoordinator.swift
-//  iOS_201
-//
-//  Created by Hyeonho on 11/5/23.
-//
-
 import Foundation
+import UIKit
+
+protocol Coordinator {
+    var navigationController: UINavigationController { get set}
+    
+    func start()
+}
+
+class AppCoordinator: Coordinator {
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        /// 첫시작은 BoardVC 차후 로그인/ 자동로그인 유무에 따라 조건추가 가능
+        goToLoginView()
+    }
+    
+    func goToLoginView() {
+        let vc = LoginViewController()
+        vc.viewmodel.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+    
+    func goToOnBoardingView() {
+        let vc = OnBoardingViewController()
+        vc.viewmodel.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+    
+    func changeToMainView() {
+        let vc = CustomTabBarViewController()
+        UIApplication.shared.keyWindow?.replaceRootViewController(vc, animated: true, completion: nil)
+    }
+    
+    func pop() {
+        navigationController.popViewController(animated: true)
+    }
+}
--- iOS_201/iOS_201/Resources/AppViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AppViewModel.swift
-//  iOS_201
-//
-//  Created by Hyeonho on 11/8/23.
-//
-
-import Foundation
